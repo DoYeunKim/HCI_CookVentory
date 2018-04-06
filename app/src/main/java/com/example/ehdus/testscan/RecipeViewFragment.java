b288da98229240270255c398aae250ec1ef6c51e
Conor Walsh <cwalsh@bowdoin.edu>
2018-04-06 14:58:23 -0400

    Moved the button back to where it goes (oops)

--- app/src/main/java/com/example/ehdus/testscan/RecipeViewFragment.java
+++ app/src/main/java/com/example/ehdus/testscan/RecipeViewFragment.java
@@ -5,21 +5,10 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class RecipeViewFragment extends Fragment {
