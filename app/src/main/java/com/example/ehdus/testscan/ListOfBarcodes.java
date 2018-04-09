353c520171c7a2ef1db4b061eab7f30d50de2b2b
gpunzalan18 <gpunzalan18@gmail.com>
2018-04-09 15:52:07 -0400

    upc lookup working, temporarily just prints out the data response

--- app/src/main/java/com/example/ehdus/testscan/ListOfBarcodes.java
+++ app/src/main/java/com/example/ehdus/testscan/ListOfBarcodes.java
@@ -1,17 +1,32 @@
 package com.example.ehdus.testscan;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.*;
 
 public class ListOfBarcodes extends AppCompatActivity {
+
+    private ArrayList<Ingredient> ingredients = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +48,20 @@ public class ListOfBarcodes extends AppCompatActivity {
         rv.setAdapter(new BarcodeAdapter(barcodeImport()));
 
         System.out.println(barcodeReceived);
+
+        String barcodeString = "031155205002,885909950805,";
+        barcodeString = barcodeString.substring(0, barcodeString.length() - 1);
+
+
+
+        List<String> barcodelist = new ArrayList<String>(Arrays.asList(barcodeString.split(",")));
+
+        for(int i = 0; i < barcodelist.size(); i++){
+            String URL = "https://api.upcitemdb.com/prod/trial/lookup?upc=" + barcodelist.get(i);
+
+            new barcodeImport().execute(URL);
+        }
+
 
 //        FloatingActionButton activityConfirmButton = findViewById(R.id.showBarcode);
 //        activityConfirmButton.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +88,8 @@ public class ListOfBarcodes extends AppCompatActivity {
                 startActivity(checkMain);
             }
         });
+
+
     }
 
     @Override
@@ -76,5 +107,73 @@ public class ListOfBarcodes extends AppCompatActivity {
         }
         return barcodes;
     }
+
+
+    private class barcodeImport extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String...params){
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect(); //connects to server and returns data as input stream
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+
+                StringBuffer buffer = new StringBuffer();
+
+                String line = "";
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line);
+                }
+
+                ingredients = new ArrayList<>();
+                String finalJson = buffer.toString();
+
+                JSONObject parentObject = new JSONObject(finalJson);
+                JSONArray parentArray = parentObject.getJSONArray("items");
+
+                Log.d("Reponse", parentArray.toString());
+                return parentArray.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+            finally {
+
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute (String result){
+            super.onPostExecute(result);
+
+        }
+    }
+
 
 }
