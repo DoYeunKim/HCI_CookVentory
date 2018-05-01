af4f2a514539a934e91a8a2f68f7f43a5aefbbc4
gpunzalan18 <gpunzalan18@gmail.com>
2018-04-30 20:35:44 -0400

    Share btn links to google, change to recipe url

--- app/src/main/java/com/example/ehdus/testscan/RecipeAdapter.java
+++ app/src/main/java/com/example/ehdus/testscan/RecipeAdapter.java
@@ -1,10 +1,13 @@
 package com.example.ehdus.testscan;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,8 +15,11 @@ import android.widget.TextView;
 //  Also implements filtering features for search
 public class RecipeAdapter extends FilterAdapter<Recipe> {
 
+    Context mContext;
+
     RecipeAdapter(Context context) {
         super(context);
+        mContext = context;
     }
 
     // Create new views (invoked by the layout manager)
@@ -34,23 +40,40 @@ public class RecipeAdapter extends FilterAdapter<Recipe> {
         ((TextView) views[0]).setText(r.getName());
         ((TextView) views[1]).setText(String.format("Ratings: %d", r.getRating()));
         ((ImageView) views[2]).setImageDrawable(r.getPic());
+
+        ImageButton favorite = (ImageButton) views[3];
+        ImageButton share = (ImageButton) views[4];
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TO DO: change str to recipe url
+                String str = "http://www.google.com";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(str));
+                mContext.startActivity(intent);
+
+            }
+        });
     }
 
     // Contains a set of views so the RecyclerView knows how to map input to display
     class ViewHolder extends FilterAdapter.CustomViewHolder {
         private final TextView mName, mDesc;
         private final ImageView mPic;
+        private final ImageButton mFav, mShare;
 
         private ViewHolder(View itemView) {
             super(itemView);
             mName = itemView.findViewById(R.id.name);
             mDesc = itemView.findViewById(R.id.desc);
             mPic = itemView.findViewById(R.id.pic);
+            mFav = itemView.findViewById(R.id.btnFav);
+            mShare = itemView.findViewById(R.id.btnShare);
         }
 
         @Override
         View[] getViews() {
-            return new View[]{mName, mDesc, mPic};
+            return new View[]{mName, mDesc, mPic, mFav, mShare};
         }
     }
 }
